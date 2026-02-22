155bc952de68cb70a17ab4faf726ef68476caf60
daraber <da6003be-s@student.lu.se>
2026-02-22 22:26:08 +0100

    Setup DI

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,10 @@
+using System.Text.Json;
+using System.Threading.Channels;
+using StackExchange.Redis;
 using Synthient.Edge.Endpoints;
+using Synthient.Edge.Models;
+using Synthient.Edge.Pipeline;
+using Synthient.Edge.Services;
 using Synthient.Edge.Utilities;
 
 var appConfig = AppConfigLoader.Load(args);
@@ -8,7 +14,56 @@ var appConfig = AppConfigLoader.Load(args);
 var builder = WebApplication.CreateSlimBuilder(args);
 builder.WebHost.UseUrls($"http://{appConfig.Server.Host}:{appConfig.Server.Port}");
 
+// Config.
 builder.Services.AddSingleton(appConfig);
+builder.Services.ConfigureHttpJsonOptions(o =>
+    o.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+);
+
+// Infra.
+builder.Services.AddSingleton<IConnectionMultiplexer>(_ => ConnectionMultiplexer.Connect(new ConfigurationOptions
+{
+    EndPoints = { appConfig.Sink.Endpoint },
+    Password = appConfig.Sink.Password,
+    Ssl = appConfig.Sink.Ssl,
+    DefaultDatabase = appConfig.Sink.Database,
+    AbortOnConnectFail = false,
+    ConnectRetry = 5,
+    ConnectTimeout = 5_000,
+    SyncTimeout = 5_000,
+}));
+
+builder.Services.AddSingleton<MmDbReader>();
+builder.Services.AddSingleton<IStringRegistry, RedisStringRegistry>();
+builder.Services.AddSingleton<IEventRepository, RedisEventRepository>();
+
+// Pipeline.
+var eventChannel = Channel.CreateBounded<ProxyEvent>(new BoundedChannelOptions(250_000)
+{
+    FullMode = BoundedChannelFullMode.DropWrite,
+    SingleWriter = true,
+    SingleReader = false
+});
+
+var bucketedEventChannel = Channel.CreateBounded<BucketedEvent>(new BoundedChannelOptions(250_000)
+{
+    FullMode = BoundedChannelFullMode.Wait,
+    SingleWriter = false,
+    SingleReader = true
+});
+
+builder.Services.AddSingleton(eventChannel.Writer);
+builder.Services.AddSingleton(eventChannel.Reader);
+builder.Services.AddSingleton(bucketedEventChannel.Writer);
+builder.Services.AddSingleton(bucketedEventChannel.Reader);
+
+builder.Services.AddHostedService<RedisPubSubSource>();
+builder.Services.AddHostedService<BucketFilter>();
+builder.Services.AddHostedService<EventSink>();
+
+// Metrics
+builder.Services.AddSingleton<MetricsReporter>();
+builder.Services.AddHostedService(sp => sp.GetRequiredService<MetricsReporter>());
 
 #endregion
 
