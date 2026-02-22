31340753108e9f6b94824d770278d3e63ad53367
daraber <da6003be-s@student.lu.se>
2026-02-22 17:55:28 +0100

    Add service for local MaxMind DB lookups

---
+++ src/Services/MmDbReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Synthient.Edge.Models.Config;
+
+namespace Synthient.Edge.Services;
+
+public class MmDbReader(AppConfig appConfig, ILogger<MmDbReader> logger) : IDisposable
+{
+    private readonly MaxMind.Db.Reader _mmDbReader = new(appConfig.Mmdb.Path);
+
+    public MmdbData Lookup(IPAddress ipAddress)
+    {
+        try
+        {
+            var mmDbData = _mmDbReader.Find<MmdbData>(ipAddress);
+            return mmDbData ?? [];
+        }
+        catch (Exception ex)
+        {
+            logger.LogWarning(ex, "MMDB lookup failed for IP {Ip}.", ipAddress);
+            return [];
+        }
+    }
+
+    public void Dispose()
+    {
+        _mmDbReader.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
